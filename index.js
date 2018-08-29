9c4214b114ef2175ed6e655ebaa1ba940fca6109
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-29 01:04:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "e68984af4783cdfe425e604d390fc92d";
 
 function handleFormSubmit(event) {
-  //handle submit event
+    console.log("== handleFormSubmit ==")
+    event.preventDefault();
+    let city = document.getElementById("city").value;
+    console.log("city:", city)
+
+    fetchCurrentWeather(city)
+    //handle submit event
 }
 
 function fetchCurrentWeather(city) {
-  //fetch current weather based on city
+    console.log("== fetchCurrentWeather ==")
+
+    let url = "https://api.openweathermap.org/data/2.5/weather?";
+    url += "q=";
+    url += city;
+    url += "&APPID=";
+    url += API_KEY;
+    console.log("url:", url)
+
+    fetch(url)
+      .then( (resp) => {
+        console.log("== then1 ==");
+        return resp.json()
+      })
+      .catch( error => {
+        console.log("== catch1 ==");
+        console.error(`Oh no! ${error}`)
+      })
+      .then( json => {
+        console.log("== then2 ==");
+        console.log(json);
+        displayCurrentWeather(json);
+      } )
+      .catch( error => {
+        console.log("== catch2 ==");
+        console.error(`Ruh-roh! Couldn't convert the json: ${error}`)
+      })
 }
 
 function displayCurrentWeather(json) {
-  //render current weather data to the DOM using provided IDs and json from API
+    console.log("== displayCurrentWeather ==")
+    //render current weather data to the DOM using provided IDs and json from API
 }
 
-
 function fetchFiveDayForecast(city) {
-  //fetch five day forecast data based on city
+    console.log("== fetchFiveDayForecast ==")
+    //fetch five day forecast data based on city
 }
 
 function displayFiveDayForecast(json) {
-  //render five day forecast data to the DOM using provided IDs and json from API
+    console.log("== displayFiveDayForecast ==")
+    //render five day forecast data to the DOM using provided IDs and json from API
 }
 
 function createChart(json) {
+  console.log("== createChart ==")
   //Bonus: render temperature chart using five day forecast data and ChartJS
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  //add event listener here for form submission
+  console.log("-- DOMContentLoaded --")
+  document.getElementById("submitBtn").addEventListener("click", handleFormSubmit)
 })
