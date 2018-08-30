d943f84f8bf4bdd746b8ba52e899dcaa224c85db
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-30 22:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,42 +2,10 @@ const API_KEY = "e68984af4783cdfe425e604d390fc92d";
 
 function handleFormSubmit(event) {
     console.log("== handleFormSubmit ==")
-    event.preventDefault();
-    let city = document.getElementById("city").value;
-    console.log("city:", city)
-
-    fetchCurrentWeather(city)
-    //handle submit event
 }
 
 function fetchCurrentWeather(city) {
     console.log("== fetchCurrentWeather ==")
-
-    let url = "https://api.openweathermap.org/data/2.5/weather?";
-    url += "q=";
-    url += city;
-    url += "&APPID=";
-    url += API_KEY;
-    console.log("url:", url)
-
-    fetch(url)
-      .then( (resp) => {
-        console.log("== then1 ==");
-        return resp.json()
-      })
-      .catch( error => {
-        console.log("== catch1 ==");
-        console.error(`Oh no! ${error}`)
-      })
-      .then( json => {
-        console.log("== then2 ==");
-        console.log(json);
-        displayCurrentWeather(json);
-      } )
-      .catch( error => {
-        console.log("== catch2 ==");
-        console.error(`Ruh-roh! Couldn't convert the json: ${error}`)
-      })
 }
 
 function displayCurrentWeather(json) {
